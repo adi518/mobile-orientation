7f32a1008e290936cf5d0544db990f3e4b1cec40
Adi Sahar <adisahar1@hotmail.com>
2018-01-22 23:03:35 +0200

    Code improve; Add Api props: 'isPortrait`, `isLandscape`; Add event subscription; Add demo, previews

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Mobile-Orientation
 Detect Mobile Portrait/Landscape on resize.
 ## Install
-```bash
+```
 npm install --save mobile-orientation
 ```
 ## Usage
@@ -10,6 +10,33 @@ import { MobileOrientation } from 'mobile-orientation'
 
 const orientation = new MobileOrientation()
 
-console.log(orientation)
-// { state: 'portrait' }
+console.log(orientation) // { state: 'portrait' }
+
+orientation.subscribe('resize', state => console.log(state)) // portrait or landscape
+orientation.subscribe('portrait', state => console.log(state)) // portrait
+orientation.subscribe('landscape', state => console.log(state)) // landscape
 ```
+Alternatively, the state can be utilized within a computed property, a la [Vue.js Computed Property](https://vuejs.org/v2/guide/computed.html).
+```js
+// Vue Component
+export default {
+    data() {
+        return {
+            orientation: new MobileOrientation()
+        }
+    },
+    computed: {
+        isPortrait() {
+            return this.orientation.isPortrait
+        }
+    }
+}
+```
+## Options
+Time to wait before invoking tests.
+* `debounceTime`: {Number} | Default: 50 | Format: Milliseconds
+
+## Preview
+<img src="portrait.png" height="400" alt="portrait">
+<br><br>
+<img src="landscape.png" width="400" alt="landscape">
--- index.js
+++ index.js
@@ -1,31 +1,49 @@
+/* eslint-disable space-before-function-paren */
+
 // https://davidwalsh.name/orientation-change
 // https://stackoverflow.com/a/9039885/4106263
+// https://developer.mozilla.org/en-US/docs/Web/API/Screen/width
 // https://stackoverflow.com/questions/9038625/detect-if-device-is-ios
 // https://developer.mozilla.org/en-US/docs/Web/API/Detecting_device_orientation
 // https://stackoverflow.com/questions/4917664/detect-viewport-orientation-if-orientation-is-portrait-display-alert-message-ad
 
 // Resources
 import debounce from 'lodash.debounce'
+import Emitter from 'es6-emitter'
+
+// Instances
+const emitter = new Emitter()
+
+// Event-Types
+const RESIZE = 'resize'
+const PORTRAIT = 'portrait'
+const LANDSCAPE = 'landscape'
 
 // Implementation
 export class MobileOrientation {
   constructor(options = {}) {
     // Setup Options
     const {
-      debounceTime = 0
+      debounceTime = 50
     } = options
 
     // Initial State
-    this.state = {}
+    this.state = null
 
     // Setup Debounce
-    this.debounced = debounce(this.detect, debounceTime)
+    this.detect = debounce(() => {
+      this._detect()
+      this.emit(RESIZE)
+    }, debounceTime)
 
     // Bind Events
-    window.addEventListener('resize', this.debounced)
+    window.addEventListener(RESIZE, this.detect)
 
     // Initial Detection
-    this.debounced()
+    this.detect()
+  }
+  get emitter() {
+    return emitter
   }
   get isMobile() {
     const tests = []
@@ -39,33 +57,52 @@ export class MobileOrientation {
     return !this.isMobile
   }
   get isPortrait() {
-    return !this.isLandscape
+    return this.state === 'portrait'
   }
   get isLandscape() {
+    return this.state === 'landscape'
+  }
+  get _isPortrait() {
+    return !this._isLandscape
+  }
+  get _isLandscape() {
     const tests = [window.screen.width > window.screen.height]
     if (window.matchMedia) {
       tests.push(window.matchMedia('all and (orientation:landscape)').matches)
     }
     return this.isTruthy(tests)
   }
+  get events() {
+    return [RESIZE, PORTRAIT, LANDSCAPE]
+  }
   isTruthy(tests = []) {
     return tests.some(result => result === true)
   }
-  detect = () => {
+  _detect = () => {
     this.detectPortrait()
     this.detectLandscape()
   }
   detectPortrait = () => {
-    if (this.isPortrait || this.isDesktop) {
+    if (this._isPortrait || this.isDesktop) {
       this.state = 'portrait'
+      this.emit(PORTRAIT)
     }
   }
   detectLandscape = () => {
-    if (this.isLandscape && this.isMobile) {
+    if (this._isLandscape && this.isMobile) {
       this.state = 'landscape'
+      this.emit(LANDSCAPE)
     }
   }
-  destroy = () => {
-    window.removeEventListener('resize', this.debounced)
+  emit(event) {
+    this.emitter.emit(event, this.state)
+  }
+  subscribe(event, callback = () => { }) {
+    if (this.events.includes(event)) {
+      this.emitter.subscribe(event, callback)
+    }
+  }
+  destroy() {
+    window.removeEventListener(RESIZE, this.detect)
   }
 }
